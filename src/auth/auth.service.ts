99c2bd1ba19aa804d9b0a59ba63b9ef3d579a8e5
D3kion <dekion.dev@gmail.com>
2020-04-28 17:03:12 +0300

    Fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,8 @@
 import { Injectable } from '@nestjs/common';
-<<<<<<< HEAD
-import { UserRepository } from './user.repository';
-import { InjectRepository } from '@nestjs/typeorm';
-
-=======
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { UserRepository } from './user.repository';
 
->>>>>>> Added User module
 @Injectable()
 export class AuthService {
   constructor(
