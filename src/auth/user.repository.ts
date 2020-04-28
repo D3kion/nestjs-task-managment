4ddfa75a1ec28d093340aced73c99b7ccb5db984
D3kion <dekion.dev@gmail.com>
2020-04-28 16:55:28 +0300

    Added User module

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,15 @@
 import { Repository, EntityRepository } from 'typeorm';
+<<<<<<< HEAD
+=======
+
+>>>>>>> Added User module
 import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+<<<<<<< HEAD
   //
+=======
+  async signUp() {}
+>>>>>>> Added User module
 }
